09569f52981c507babd468fbeb24d1e128af8aed
Burhaan-U-Deen wani <burhaanwani823@gmail.com>
2025-08-04 12:46:16 +0530

    add google authentication

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -2,20 +2,21 @@ import { NextFunction, Response, Request } from "express";
 import asyncHandler from "../middlewares/asyncHandler.middleware";
 import config from "../config/app.config";
 import { UserDocument } from "../models/user.model";
+import mongoose, { Schema } from "mongoose";
 
-// declare module "express-serve-static-core" {
-//     interface Request {
-//         user?: UserDocument;
-//     }
-// }
+export interface AuthRequest extends Request {
+    user?: UserDocument;
+}
+
 export const googleLoginCallback = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-        if (!req.user) {
+        const user = req.user as UserDocument | undefined;
+        if (!user) {
             return res.redirect(
                 `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`
             );
         }
-        const currentWorkspaceId = req.user.currentWorkspace;
+        const currentWorkspaceId = user.currentWorkspace;
 
         if (!currentWorkspaceId) {
             console.log("Redirecting because currentWorkspaceId is falsy.");
